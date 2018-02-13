9660f1e3e0b661121c34150cff9b9c0c37775c6e
Moises Dobarganes <designerexpert@gmail.com>
2018-02-13 13:12:44 -0500

    Init

---
+++ src/dummy-data.js
@@ -0,0 +1,105 @@
+const dummyData = [
+    {
+        username: "philzcoffee",
+        thumbnailUrl: 'https://www.philzcoffee.com/c.1145465/sca-dev-vinson//img/philz-logo-web.png',
+        imageUrl: 'https://instagram.fmia1-2.fna.fbcdn.net/vp/9fa04b396e0afbff229574a1fb9d21a1/5B03D086/t51.2885-15/s640x640/sh0.08/e35/26871346_131178197695364_8578200041267658752_n.jpg',
+        likes: 400,
+        timestamp: "July 17th 2017, 12:42:40 pm",
+        comments: [
+            {
+                username: "philzcoffee",
+                text: "We've got more than just delicious coffees to offer at our shops!"
+            },
+            {
+                username: "biancasaurus",
+                text: "Looks delicious!"
+            },
+            {
+                username: "martinseludo",
+                text: "Can't wait to try it!"
+            }
+        ]
+    },
+    {
+
+        username: "twitch",
+        thumbnailUrl: 'https://instagram.fmia1-2.fna.fbcdn.net/vp/34d83d4be4de74c2fd4f41f1735b801c/5B0E3B40/t51.2885-19/10684037_1512272482349821_444823433_a.jpg',
+        imageUrl: 'https://instagram.fmia1-2.fna.fbcdn.net/vp/4040647a3d0c0420abb34ae5e1500e59/5B1EC943/t51.2885-15/e35/27580069_1625157204237833_9090302512526786560_n.jpg',
+        likes: 4307,
+        timestamp: "July 15th 2017, 03:12:09 pm",
+        comments: [
+            {
+                username: "twitch",
+                text: "Epic Street Fighter action here in Las Vegas at #EVO2017!"
+            },
+            {
+                username: "michaelmarzetta",
+                text: "Omg that match was crazy"
+            },
+            {
+                username: "themexican_leprechaun",
+                text: "What a setup"
+            },
+            {
+                username: "dennis_futbol",
+                text: "It that injustice"
+            },
+            {
+                username: "dennis_futbol",
+                text: "Is"
+            }
+        ]
+    },
+    {
+        username: "playhearthstone",
+        thumbnailUrl: 'https://instagram.fmia1-2.fna.fbcdn.net/vp/be7e11c58e41807b12c7a789da6a1d4f/5B232DA7/t51.2885-19/s150x150/13398400_140638743023210_310840336_a.jpg',
+        imageUrl: 'https://instagram.fmia1-2.fna.fbcdn.net/vp/0e575f6e69a076f845880c53a180b644/5B090AE7/t51.2885-15/s640x640/sh0.08/e35/21042747_1310984049023459_8114276593284677632_n.jpg',
+        likes: 5306,
+        timestamp: "July 14th 2017, 10:04:08 am",
+        comments: [
+            {
+                username: "playhearthstone",
+                text: "Power alone is not to be feared. Fear instead those who wield it! #FrozenThrone #Expansion #DeathKnights"
+            },
+            {
+                username: "tapmelon",
+                text: "Wish that death knight could be added as a new playable class in this expansion."
+            },
+            {
+                username: "micpetboudreau",
+                text: "Can't wait"
+            },
+            {
+                username: "awaywetravel",
+                text: "I <3 Hearthstone."
+            },
+            {
+                username: "awesomebt28",
+                text: "I like how gul'dan looks so old and useless"
+            }
+        ]
+    },
+    {
+        username: "designerexpert",
+        thumbnailUrl: 'https://instagram.fmia1-2.fna.fbcdn.net/t51.2885-19/s150x150/13686882_1680049718987637_47605772_a.jpg',
+        imageUrl: 'https://instagram.fmia1-2.fna.fbcdn.net/t51.2885-15/e35/14276614_1566366740338319_1943654623_n.jpg',
+        likes: 9001,
+        timestamp: "July 17th 2017, 12:42:40 pm",
+        comments: [
+            {
+                username: "designerexpert",
+                text: "Its Overninethousand! likes!"
+            },
+            {
+                username: "GokuSan",
+                text: "Begeta what is his powerlevel!"
+            },
+            {
+                username: "martinseludo",
+                text: "Can't wait to try it!"
+            }
+        ]
+    },
+];
+
+export default dummyData;
